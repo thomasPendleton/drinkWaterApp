b79825d627e45de5250550dae3846d44605ee37a
Thomas Pendleton <thomas6576@yahoo.com>
2022-02-07 22:02:29 -0800

    some javascript added

---
+++ script.js
@@ -0,0 +1,35 @@
+const smallCups = document.querySelectorAll('.small-cup')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
+
+smallCups.forEach((cup, i) => {
+  cup.addEventListener('click', (e) => {
+    highlightCups(i)
+  })
+})
+
+function highlightCups(index) {
+  if (
+    smallCups[index].classList.contains('drank') &&
+    !smallCups[index].nextElementSibling.classList.contains('drank')
+  ) {
+    index--
+  }
+
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) {
+      cup.classList.add('drank')
+    } else {
+      cup.classList.remove('drank')
+    }
+  })
+  updateBigCup()
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.small-cup.drank').length
+  
+}
